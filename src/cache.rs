de618f08fab7de8d64614abf5e627df784ea93a1
Quentin Moss <quentin.moss@protonmail.com>
2023-06-30 19:02:09 -0700

    Add comments because i'm definitely going to forget how this works

--- src/cache.rs
+++ src/cache.rs
@@ -22,11 +22,18 @@ impl OriginCache {
 
 
     pub async fn refresh(&self) -> Result<(), AppError> {
+        // Fetch the latest origin data from the database using the provided SqlitePool
         let new_origins = list_origins(&self.pool).await?;
+
+        // Create a new HashMap to store the updated origin data
         let mut map = HashMap::new();
+
+        // Iterate over the fetched origins and insert them into the map
         for origin in new_origins {
             map.insert(origin.domain.clone(), origin);
         }
+
+        // Update the cache by acquiring a write lock and replacing the HashMap
         *self.origins.write() = map;
         Ok(())
     }
@@ -34,13 +41,18 @@ impl OriginCache {
     pub async fn get(&self, domain: &str) -> Option<Origin> {
         tracing::info!("Get called on cache for domain: {}", domain);
         let origins = self.origins.read();
+
+        // Look up domain in the cache and clone if found
         let result = origins.get(domain).cloned();
 
+        // Mostly for development, but also useful if you want to see how often the cache is hit
         if result.is_some() {
             tracing::info!("Found origin in cache");
         } else {
             tracing::info!("Origin not found in cache");
         }
+
+        // Return the result if found, otherwise None
         result
     }
 }
